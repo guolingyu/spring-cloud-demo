d6fe8014f402ddd3d98ec0eff0b3577274c762ab
guolingyu <gly199@gmail.com>
2017-01-22 17:04:44 +0800

    提交 eureka 的代码

--- service-api/src/main/java/com/syswin/springcloud/service/Application.java
+++ service-api/src/main/java/com/syswin/springcloud/service/Application.java
@@ -3,12 +3,14 @@ package com.syswin.springcloud.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by GuoLingyu on 17/1/22.
  */
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) throws Exception {
---
+++ service-discovery/src/main/java/com/syswin/springcloud/discovery/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.syswin.springcloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by GuoLingyu on 17/1/22.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
